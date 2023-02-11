125a1ff547abead8a1b8c33623476f1f812c8273
olivia <olivia.brantley9@gmail.com>
2023-02-11 11:34:29 -0800

    created working api link

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('hi');
+
+
+let latitude = 34.05;
+let longitude = -118.24;
+let cityName = 'los angeles'
+let limit = 1;
+async function getForcast(){
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=47d07c43f6e4181dbf36a31437ebe789&units=imperial`)
+    let weatherForcast = await response.json();
+    console.log(weatherForcast);
+}
+
+getForcast();
+
+
